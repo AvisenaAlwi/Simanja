7e3624df7a9c6dab91e737846806b3feb4539776
AvisenaAlwi <avisena.abdillah98@gmail.com>
2019-07-21 09:46:46 +0700

    Fix error page

--- resources/views/errors/401.blade.php
+++ resources/views/errors/401.blade.php
@@ -1,11 +1,11 @@
-@extends('errors::illustrated-layout')
+@extends('errors.illustrated-layout')
 
 @section('code', '401')
-@section('title', __('Unauthorized'))
+@section('title', 'Tidak Sah')
 
 @section('image')
     <div style="background-image: url({{ asset('/svg/403.svg') }});" class="absolute pin bg-cover bg-no-repeat md:bg-left lg:bg-center">
     </div>
 @endsection
 
-@section('message', __('Sorry, you are not authorized to access this page.'))
+@section('message', $exception->getMessage() ?: 'Anda tidak sah mengakses halaman ini')
--- resources/views/errors/500.blade.php
+++ resources/views/errors/500.blade.php
@@ -1,11 +1,11 @@
-@extends('errors::illustrated-layout')
+@extends('errors.illustrated-layout')
 
 @section('code', '500')
-@section('title', __('Error'))
+@section('title', 'Error')
 
 @section('image')
     <div style="background-image: url({{ asset('/svg/500.svg') }});" class="absolute pin bg-cover bg-no-repeat md:bg-left lg:bg-center">
     </div>
 @endsection
 
-@section('message', __('Whoops, something went wrong on our servers.'))
+@section('message', $exception->getMessage() ?: 'Aduh, terjadi kesalahan pada server')
